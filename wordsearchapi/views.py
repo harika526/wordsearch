7b939cd80b19f72398e60fc4683537a32376f944
harika <harikacherukupalli1@gmail.com>
2019-08-02 23:42:30 +0530

    added comments

--- wordsearchapi/views.py
+++ wordsearchapi/views.py
@@ -17,19 +17,36 @@ def home(request):
 
 
 def search_results(search_string):
-    word = Words.objects.filter(word=search_string)[:1]
-    if word:
+    """ Queries the database for results, passes the queryset to serializer and returns the result """
+
+    # initialize empty list to store the serialized result
+    result = []
+
+    # obtain exact match to search string
+    word = Words.objects.filter(word=search_string).values("word")
+    result.extend(WordSerializer(word, many=True).data)
+
+    # if an exact match is found remaining 24 results should be queried, else 25 results should be queried
+    if result:
         limit = 24
     else:
         limit = 25
-    wordlist_start = Words.objects.filter(~Q(word=search_string), word__startswith=search_string)[:limit]
-    wordlist = word.union(wordlist_start)
-    count = len(wordlist)
+
+    # pattern to match words staring with search string and not exact match
+    pattern = r'^{}.+'.format(search_string)
+    wordlist_start = Words.objects.filter(word__iregex=pattern).values("word")[:limit]
+    result.extend(WordSerializer(wordlist_start, many=True).data)
+    count = len(result)
+
+    # if results are less than 25 then query the words containing search
+    # string in middle to fill the remaining count, else not required
     if count < 25:
         limit = 25 - count + 1
-        wordlist_contains = Words.objects.filter(word__icontains=search_string)[:limit]
-        wordlist = wordlist.union(wordlist_contains)
-    return wordlist
+        pattern = r'.+{}.*'.format(search_string)
+        wordlist_contains = Words.objects.filter(word__iregex=pattern).values("word")[:limit]
+        result.extend(WordSerializer(wordlist_contains, many=True).data)
+
+    return result
 
 
 @api_view(['GET'])
@@ -41,9 +58,10 @@ def get_word(request):
             search_string = request.GET['word']
             print("building search results...")
             start_time = time.time()
+
+            # call search_results() function to get the results
             word_list = search_results(search_string)
             print(time.time() - start_time)
-            serializer = WordSerializer(word_list, many=True)
-            return Response(serializer.data)
+            return Response(word_list)
         else:
             return HttpResponse("Use the following endpoint to search for word: <br/> GET /search?word=&lt;input&gt;")
